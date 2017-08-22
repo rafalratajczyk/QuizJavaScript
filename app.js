93e532c0b08fef6d6ea22b816bbca80ddb4e87c1
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-08-22 19:08:04 +0200

    questions with answer

---
+++ app.js
@@ -0,0 +1,6 @@
+var questions = [
+    new Question("Who is Jon Snow mother?", ["Lyana Stark", "Catelyn Stark", "Elia Martell", "Cersei Lannister"], "Lyana Stark"),
+    new Question("What does Tyrion murder his father with?", ["Poison", "Crossbow", "Sword", "Mace"], "Crossbow"),
+    new Question(" Who is known as The Kingslayer?", ["Sandor Clegane", "Joffrey Baratheon", "Eddard Stark", "Jaime Lannister"], "Jaime Lannister"),
+    new Question("Who said this memorable line: 'You know nothing, Jon Snow'.", ["Daenerys Targaryen", "Cersei Lannister", "Ygritte", "Podrick Payne"], "Ygritte")
+];
